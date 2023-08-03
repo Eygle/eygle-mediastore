1d06f6f300130f97dc7e8c1716bbb3f8c28aa373
Eygle <cavalec.johan@gmail.com.com>
2023-08-03 22:19:56 +0200

    chore(prettier): edit conf

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,34 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:import/recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier',
   ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
+    'import/order': 'error',
+    'vue/max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreUrls: true,
+        ignoreComments: true,
+        ignoreStrings: false,
+        ignoreHTMLAttributeValues: true,
+      },
+    ],
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
 }
