6c2e7cb1f68c634833c37865fdfd200dad3536e4
Eygle <cavalec.johan@gmail.com.com>
2023-08-03 21:29:11 +0200

    feat(tag): add base

---
+++ src/tag/tag.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Tag } from "./tag.entity";
+import { TagService } from './tag.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Tag])],
+  exports: [TypeOrmModule],
+  providers: [TagService]
+})
+export class TagModule {}
