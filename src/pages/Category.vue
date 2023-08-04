00fb78fed5855b734f33460ebc7e6cf033bf9a8a
Eygle <cavalec.johan@gmail.com.com>
2023-08-04 16:21:30 +0200

    refact(category): use MediaCard

--- src/pages/Category.vue
+++ src/pages/Category.vue
@@ -2,43 +2,21 @@
 import { useMediaGroupApi } from '@/composables/media-group-api'
 import { onBeforeMount, ref } from 'vue'
 import { useRoute } from 'vue-router'
-import { MediaDto } from '@/dto/MediaDto'
+import MediaGroupDetails from '@/components/commons/MediaGroupDetails.vue'
+import { MediaGroupDto } from '@/dto/MediaGroupDto'
 
-const { fetchMedias } = useMediaGroupApi()
+const { getMediaGroupById } = useMediaGroupApi()
 
-const medias = ref<MediaDto[]>([])
+const category = ref<MediaGroupDto | null>(null)
 
 onBeforeMount(async () => {
-  medias.value = await fetchMedias(+useRoute().params.id)
+  category.value = await getMediaGroupById(+useRoute().params.id)
 })
 </script>
 
 <template>
   <v-container>
-    <h2>Best by categories</h2>
-    <v-card v-for="media of medias" :key="media.id" class="mt-4">
-      <v-card-title>{{ media.title }}</v-card-title>
-      <v-card-text>
-        <v-chip v-for="tag of media.tags" :key="`tag-${tag.id}`" class="mr-2 mb-2">
-          {{ tag.title }}
-        </v-chip>
-        <v-card class="mt-4" color="grey-darken-3" variant="flat">
-          <v-card-text>
-            <pre class="text-caption">{{ media.files.join('\n') }}</pre>
-          </v-card-text>
-        </v-card>
-
-        <div v-if="media.starring.length" class="mt-4">
-          Starring:
-          <router-link v-for="star of media.starring" :key="`star-${star.id}`" to="">
-            {{ star.name }}
-          </router-link>
-        </div>
-
-        <div class="mt-4 pl-2 border-s-lg" v-if="media.comment">
-          {{ media.comment }}
-        </div>
-      </v-card-text>
-    </v-card>
+    <div v-if="!category"></div>
+    <MediaGroupDetails v-else :group="category" />
   </v-container>
 </template>
