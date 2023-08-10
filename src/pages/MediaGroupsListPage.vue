df8ce3f7a664cd9c0a10db71e816f559416ca7e3
Eygle <cavalec.johan@gmail.com.com>
2023-08-10 18:16:13 +0200

    feat(cache): add caching

--- src/pages/MediaGroupsListPage.vue
+++ src/pages/MediaGroupsListPage.vue
@@ -1,17 +1,17 @@
 <script lang="ts" setup>
-import { useApi } from '@/composables/api'
 import { onBeforeMount, ref } from 'vue'
-import { Field } from '@/types/Field'
 import { MediaGroupDto } from '@/dto/MediaGroupDto'
 import MediaGroupCard from '@/components/commons/MediaGroupCard.vue'
 import UpsertMediaGroupDialog from '@/components/UpsertMediaGroupDialog.vue'
 import { useRoute, useRouter } from 'vue-router'
-import { RouteName } from '@/types/RouteName'
 import { useDialogs } from '@/composables/dialogs'
+import { useCache } from '@/composables/cache'
+import { useApi } from '@/composables/api'
 
 const router = useRouter()
 const route = useRoute()
 const { fetchMediaGroups } = useApi()
+const { getCached } = useCache()
 const { openMediaGroupDialog } = useDialogs()
 
 const groups = ref<MediaGroupDto[]>([])
@@ -19,9 +19,9 @@ const loading = ref(false)
 
 onBeforeMount(() => reload())
 
-async function reload() {
+async function reload(forceRefresh = false) {
   loading.value = true
-  groups.value = await fetchMediaGroups(route.meta.field!)
+  groups.value = await getCached(route.meta.field, () => fetchMediaGroups(route.meta.field), forceRefresh)
   loading.value = false
 }
 
