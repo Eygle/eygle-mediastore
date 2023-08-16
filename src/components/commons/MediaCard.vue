3cd276a820d7fcda1740ea2f7b409b47cdb663e6
Eygle <cavalec.johan@gmail.com.com>
2023-08-16 21:39:05 +0200

    refact(Cards): anim edition from right

--- src/components/commons/MediaCard.vue
+++ src/components/commons/MediaCard.vue
@@ -15,65 +15,71 @@ defineProps<{ media: MediaDto; noAction?: boolean }>()
   <v-card>
     <v-hover :disabled="noAction">
       <template #default="{ isHovering, props }">
-        <v-card-title class="d-flex justify-space-between" v-bind="props">
-          <p class="text-truncate">{{ media.title }}</p>
-          <div>
-            <v-tooltip v-if="media.toSee" text="To see">
-              <template #activator="{ props }">
-                <v-icon icon="mdi-eye" color="blue" class="ml-2" v-bind="props" />
-              </template>
-            </v-tooltip>
-            <v-tooltip v-if="media.isBest" text="Best">
-              <template #activator="{ props }">
-                <v-icon icon="mdi-star" color="primary" class="ml-2" v-bind="props" />
-              </template>
-            </v-tooltip>
-          </div>
-        </v-card-title>
-        <v-card-text v-bind="props" class="pt-4">
-          <TagChips class="pb-4 mt-n2" :parent="media" />
-
-          <v-card v-if="media.files.length" class="d-flex" color="grey-darken-3" variant="flat">
-            <v-card-text class="pb-2">
-              <div v-for="file of media.files" class="d-flex justify-space-between mb-2">
-                <p dir="rtl" class="text-caption text-truncate">{{ file.substring(1) }}</p>
-                <v-tooltip text="Copied !" open-on-click :open-on-hover="false" close-on-content-click>
+        <div class="d-flex align-center" v-bind="props">
+          <div class="flex-grow-1">
+            <v-card-title class="d-flex justify-space-between">
+              <p class="text-truncate">{{ media.title }}</p>
+              <div>
+                <v-tooltip v-if="media.toSee" text="To see">
                   <template #activator="{ props }">
-                    <v-icon icon="mdi-content-copy" class="ml-2" @click="copy(file)" v-bind="props" />
+                    <v-icon icon="mdi-eye" color="blue" class="ml-2" v-bind="props" />
+                  </template>
+                </v-tooltip>
+                <v-tooltip v-if="media.isBest" text="Best">
+                  <template #activator="{ props }">
+                    <v-icon icon="mdi-star" color="primary" class="ml-2" v-bind="props" />
                   </template>
                 </v-tooltip>
               </div>
+            </v-card-title>
+            <v-card-text class="pt-4">
+              <TagChips class="pb-4 mt-n2" :parent="media" />
+
+              <v-card v-if="media.files.length" class="d-flex" color="grey-darken-3" variant="flat">
+                <v-card-text class="pb-2">
+                  <div v-for="file of media.files" class="d-flex justify-space-between mb-2">
+                    <p dir="rtl" class="text-caption text-truncate">{{ file.substring(1) }}</p>
+                    <v-tooltip text="Copied !" open-on-click :open-on-hover="false" close-on-content-click>
+                      <template #activator="{ props }">
+                        <v-icon icon="mdi-content-copy" class="ml-2" @click="copy(file)" v-bind="props" />
+                      </template>
+                    </v-tooltip>
+                  </div>
+                </v-card-text>
+              </v-card>
+
+              <div v-if="media.starring?.length" class="mt-4">
+                Starring:
+                <router-link
+                  v-for="star of media.starring"
+                  :key="`star-${star.id}`"
+                  :to="{
+                    name: star.field === Field.Profile ? RouteName.Profile : RouteName.Star,
+                    params: { id: star.id },
+                  }">
+                  {{ star.name }}
+                </router-link>
+              </div>
+
+              <div v-if="media.progress.every(Boolean)" class="text-center">
+                <v-progress-linear
+                  :model-value="stringToDuration(media.progress[0])"
+                  :max="stringToDuration(media.progress[1])"
+                  class="mb-2" />
+                {{ media.progress[0] }} / {{ media.progress[1] }}
+              </div>
+
+              <div class="mt-4 pl-2 border-s-lg text-pre" v-if="media.comment">
+                {{ media.comment }}
+              </div>
             </v-card-text>
-          </v-card>
-
-          <div v-if="media.starring?.length" class="mt-4">
-            Starring:
-            <router-link
-              v-for="star of media.starring"
-              :key="`star-${star.id}`"
-              :to="{
-                name: star.field === Field.Profile ? RouteName.Profile : RouteName.Star,
-                params: { id: star.id },
-              }">
-              {{ star.name }}
-            </router-link>
           </div>
-
-          <div v-if="media.progress.every(Boolean)" class="text-center">
-            <v-progress-linear
-              :model-value="stringToDuration(media.progress[0])"
-              :max="stringToDuration(media.progress[1])"
-              class="mb-2" />
-            {{ media.progress[0] }} / {{ media.progress[1] }}
-          </div>
-
-          <div class="mt-4 pl-2 border-s-lg text-pre" v-if="media.comment">
-            {{ media.comment }}
-          </div>
-        </v-card-text>
-        <v-card-actions v-if="isHovering" v-bind="props" class="d-flex justify-end">
-          <v-btn variant="text" @click="openMediaDialog(media)">Edit</v-btn>
-        </v-card-actions>
+          <v-expand-x-transition>
+            <div v-if="isHovering">
+              <v-btn icon="mdi-pencil" size="x-large" variant="flat" class="mx-4" @click="openMediaDialog(media)" />
+            </div>
+          </v-expand-x-transition>
+        </div>
       </template>
     </v-hover>
   </v-card>
--- src/components/commons/MediaGroupCard.vue
+++ src/components/commons/MediaGroupCard.vue
@@ -15,24 +15,37 @@ defineProps<{ group: MediaGroupDto; noAction?: boolean }>()
     :class="{ trimmed: group.trimmed, 'to-tag': group.toTag }">
     <v-hover :disabled="noAction">
       <template #default="{ isHovering, props }">
-        <v-card-title class="d-flex align-center" v-bind="props">
-          {{ group.name }}
-          <span class="text-caption ml-2" v-if="group.count || group.total">({{ group.count || group.total }})</span>
-          <v-spacer />
-          <div v-if="group.nbToSee" class="ml-4 d-flex align-center text-secondary">
-            <v-icon icon="mdi-eye" size="22" class="mr-1" /> {{ group.nbToSee }}
+        <div class="d-flex align-center" v-bind="props">
+          <div class="flex-grow-1">
+            <v-card-title class="d-flex align-center">
+              {{ group.name }}
+              <span class="text-caption ml-2" v-if="group.count || group.total">
+                ({{ group.count || group.total }})
+              </span>
+              <v-spacer />
+              <div v-if="group.nbToSee" class="ml-4 d-flex align-center text-secondary">
+                <v-icon icon="mdi-eye" size="22" class="mr-1" /> {{ group.nbToSee }}
+              </div>
+              <div v-if="group.nbBest" class="ml-4 d-flex align-center text-primary">
+                <v-icon icon="mdi-star" size="22" class="mr-1" /> {{ group.nbBest }}
+              </div>
+            </v-card-title>
+            <v-card-text :class="{ 'pb-0': !group.tags?.length }">
+              <TagChips :parent="group" />
+              <slot />
+            </v-card-text>
           </div>
-          <div v-if="group.nbBest" class="ml-4 d-flex align-center text-primary">
-            <v-icon icon="mdi-star" size="22" class="mr-1" /> {{ group.nbBest }}
-          </div>
-        </v-card-title>
-        <v-card-text :class="{ 'pb-0': !group.tags?.length }" v-bind="props">
-          <TagChips :parent="group" />
-          <slot />
-        </v-card-text>
-        <v-card-actions v-if="isHovering" v-bind="props" class="d-flex justify-end">
-          <v-btn variant="text" @click.prevent="openMediaGroupDialog(group)">Edit</v-btn>
-        </v-card-actions>
+          <v-expand-x-transition>
+            <div v-if="isHovering">
+              <v-btn
+                icon="mdi-pencil"
+                size="x-large"
+                variant="flat"
+                class="mx-4"
+                @click.prevent="openMediaGroupDialog(group)" />
+            </div>
+          </v-expand-x-transition>
+        </div>
       </template>
     </v-hover>
   </v-card>
