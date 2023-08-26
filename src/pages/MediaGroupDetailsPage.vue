93f5e7cfd1001f8782e93a0eb1b9c154beba4b76
Eygle <cavalec.johan@gmail.com.com>
2023-08-26 15:35:09 +0200

    refact(components): remove details components

--- src/pages/MediaGroupDetailsPage.vue
+++ src/pages/MediaGroupDetailsPage.vue
@@ -3,11 +3,12 @@ import { useApi } from '@/composables/api'
 import { onBeforeMount, provide, ref } from 'vue'
 import { MediaGroupDto } from '@/dto/MediaGroupDto'
 import { useRoute, useRouter } from 'vue-router'
-import MediaGroupDetails from '@/components/MediaGroupDetails.vue'
 import UpsertMediaGroupDialog from '@/components/dialogs/UpsertMediaGroupDialog.vue'
 import UpsertMediaDialog from '@/components/dialogs/UpsertMediaDialog.vue'
 import { useDialogs } from '@/composables/dialogs'
 import { Provide } from '@/types/Provide'
+import TagChips from '@/components/TagChips.vue'
+import MediaCard from '@/components/MediaCard.vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -42,7 +43,45 @@ async function reload() {
     </div>
     <template v-else>
       <div v-if="!group"></div>
-      <MediaGroupDetails v-else :group="group" />
+      <div v-else>
+        <h2 class="d-flex">
+          {{ group.name }}
+          <span v-if="group.count || group.total" class="d-flex align-center ml-2 text-grey">
+            <span>({{ group.count || group.total }}</span>
+            <span v-if="group.count" class="text-caption ml-2">/ {{ group.total }}</span>
+            <span>)</span>
+          </span>
+          <v-spacer />
+          <v-tooltip v-if="group.toFollow" text="To follow">
+            <template #activator="{ props }">
+              <v-icon icon="mdi-youtube-subscription" color="primary" v-bind="props" />
+            </template>
+          </v-tooltip>
+          <v-tooltip v-if="group.toTag" text="To tag">
+            <template #activator="{ props }">
+              <v-icon icon="mdi-tag-plus" color="orange-lighten-2" class="ml-2" v-bind="props" />
+            </template>
+          </v-tooltip>
+          <v-tooltip v-if="group.trimmed" text="Trimmed">
+            <template #activator="{ props }">
+              <v-icon icon="mdi-content-cut" color="green-lighten-2" class="ml-2" v-bind="props" />
+            </template>
+          </v-tooltip>
+        </h2>
+        <TagChips :parent="group" class="mt-8" />
+
+        <div class="mt-8">
+          <MediaCard v-for="media of group.media" :key="media.id" :media="media" class="mt-4" />
+        </div>
+
+        <a v-if="group.externalLink" :href="group.externalLink" target="_blank" class="d-block mt-8">{{
+          group.externalLink
+        }}</a>
+
+        <div class="mt-8 pl-2 border-s-lg text-pre" v-if="group.comment">
+          {{ group.comment }}
+        </div>
+      </div>
       <UpsertMediaGroupDialog v-if="group" :source="group" @saved="(data) => (group = data)" />
       <UpsertMediaDialog v-if="group" :parent="group" @saved="reload" />
       <v-btn icon="mdi-plus" color="primary" class="fab top" size="large" @click="openMediaDialog()" />
